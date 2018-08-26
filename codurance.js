3f0ee30e2d24318e2ffd2d107a0b52930e63eee9
Ali Khalil <ali_khalil@hotmail.co.uk>
2018-08-26 21:27:51 +0100

    Implemented read function

--- codurance.js
+++ codurance.js
@@ -11,6 +11,38 @@ class User {
         let timestamp = Date.now()
         this.messages.unshift({[timestamp]: message})
     }
+
+    time (timestamp) {
+        let CurrentTime = Date.now();
+        let secondsDifference = Math.floor((CurrentTime-timestamp) / 1000)
+        if (secondsDifference >= 120) {
+            secondsDifference = Math.floor(secondsDifference / 60)
+            return `${secondsDifference} minutes ago`
+        }  else if (secondsDifference >= 60) {
+            secondsDifference = Math.floor(secondsDifference / 60)
+            return `${secondsDifference} minute ago`
+        }  else if (secondsDifference > 1 ) {
+            return `${secondsDifference} seconds ago`
+        } else if (secondsDifference === 1){
+            return `${secondsDifference} second ago`
+        } 
+        return 'just now'
+    }
+
+    read (UserMessages = this.messages){
+        let readMessages = []
+        UserMessages.forEach(m=>{
+            let message = Object.values(m);
+            let timestamp = Object.keys(m);
+            readMessages.push(`${message} (${this.time(...timestamp)})`)
+        })
+        return readMessages.join(`\n`)
+    }
+
+
+
+
+
 }
 
 // Users['Alice'] = new User('Alice')
